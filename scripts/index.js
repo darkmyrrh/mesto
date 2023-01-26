d3b7b73c2555c9c406a30891b682f92ded512f25
Светлана Суворова <myrrh@myrrh.ru>
2023-01-27 00:15:09 +0300

    Feat: добавить дополнительный функционал JS, настроить плавность открытия попапов, добавить шаблон для увеличенных изображений

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,16 @@
 let editProfile = document.querySelector(".profile-info__edit-button");
-let popup = document.querySelector(".popup");
-const formEdit = document.forms.editprofile;
-const formAdd = document.forms.addnew;
+let popups = document.querySelectorAll(".popup");
 let profileName = document.querySelector(".profile-info__name");
 let profileDescription = document.querySelector(".profile-info__description");
 let nameInput = document.querySelector(".form__item_el_name");
 let jobInput = document.querySelector(".form__item_el_job");
-let closeButton = document.querySelector(".popup__close");
+let closeButtons = document.querySelectorAll(".popup__close");
 let formElement = document.querySelector(".popup__container");
 const addButton = document.querySelector(".profile__add-button");
-const popupEdit = document.querySelector("")
-
+const popupEdit = document.querySelector(".popup_edit");
+const popupAdd = document.querySelector(".popup_add");
 const elements = document.querySelector(".elements");
+const deleteButtons = document.querySelectorAll(".element__delete-button");
 const initialCards = [{name: "Энгельс", link: "images/elements-engels.jpg"},
 {name: "Сочи", link: "images/elements-sochi.jpg"},
 {name: "Санкт-Петербург", link: "images/elements-stpetersburg.jpg"},
@@ -21,21 +20,56 @@ const initialCards = [{name: "Энгельс", link: "images/elements-engels.jpg
 
 const elementTemplate = document.querySelector(".element-template").content;
 
-function openPopup() {
-    popup.classList.add("popup_opened");
+initialCards.forEach(function (element) {
+    const cardElement = elementTemplate.cloneNode(true);
+    cardElement.querySelector(".element__image").src = element.link;
+    cardElement.querySelector(".element__title").textContent = element.name;
+    cardElement.querySelector(".element__like-button").addEventListener("click", function(evt) {
+        evt.target.classList.toggle("element__like-button_active");
+    });
+
+    elements.append(cardElement);
+
+});
+
+function openPopup(popup) {
+    popup.classList.add("popup_opened");    
+};
+
+
+function editUser() {
+    openPopup(popupEdit);
     nameInput.value = profileName.textContent;
     jobInput.value = profileDescription.textContent;
-};
+}
 
-editProfile.addEventListener("click", openPopup);
-addButton.addEventListener("click", openPopup);
+editProfile.addEventListener("click", editUser);
+
+function addNew() {
+    openPopup(popupAdd);
+    
+        /*const newCard = elementTemplate.cloneNode(true);
+        newCard.querySelector(".element__image").src = document.querySelector(".form__item_el_link").value;
+        newCard.querySelector(".element__title").textContent = document.querySelector(".form__item_el_place").value;
+        elements.prepend(newCard);   */
+} 
+
+
+
+addButton.addEventListener("click", addNew);
 
 function closePopup() {
-    popup.classList.remove("popup_opened");
+    popups.forEach (function (popup) {
+        popup.classList.remove("popup_opened");
+    });
 };
 
-closeButton.addEventListener("click", closePopup);
-
+closeButtons.forEach(function (closeButton){
+    closeButton.addEventListener("click", function() {
+        const activePopup = closeButton.closest(".popup");
+        closePopup(activePopup);
+    });
+});
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
@@ -46,21 +80,12 @@ function handleFormSubmit (evt) {
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-initialCards.forEach(function (element) {
-    const cardElement = elementTemplate.cloneNode(true);
-    cardElement.querySelector(".element__image").src = element.link;
-    cardElement.querySelector(".element__title").textContent = element.name;
-    cardElement.querySelector(".element__like-button").addEventListener("click", function(evt) {
-        evt.target.classList.toggle("element__like-button_active");
-    });
-
-    const deleteButton = cardElement.querySelector(".element__delete-button").addEventListener("click", function() {
-        const elementItem = deleteButton.closest(".element");
-        elementItem.remove();
-    });
-
-    elements.append(cardElement);
-
+deleteButtons.forEach(function (deleteButton) {
+    deleteButton.addEventListener("click", function() {
+    const elementItem = deleteButton.closest(".element");
+    elementItem.remove();
+    
+}); console.log('card deleted?');
 });
 
 
